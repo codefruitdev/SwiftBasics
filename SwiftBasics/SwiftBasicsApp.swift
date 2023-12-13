aa01df06813bf1cdc62153a3ea2b7026014a8bd1
tplesetz <12537305+tplesetz@users.noreply.github.com>
2023-12-13 16:53:04 -0600

    Initial Commit

---
+++ SwiftBasics/SwiftBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBasicsApp.swift
+//  SwiftBasics
+//
+//  Created by Tyler Plesetz on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
